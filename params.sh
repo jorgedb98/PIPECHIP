3cf298694e470f669cd53f93cfbd9253554463b1
saracart <saracart>
2019-11-17 11:08:41 +0100

    comienzo fichero params

--- params.sh
+++ params.sh
@@ -1,1 +1,9 @@
+working_directory: /home/sarajorge/BAG2019/tarea_1/PIPECHIP
+number_of_samples: 4
+genome: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/genome/genome.fa
+annotation: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/annotation/annotation.fa
+sample_1: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/samples/sample_1/sample_1.fastq.gz
+sample_2: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/samples/sample_2/sample_2.fastq.gz
+sample_3: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/samples/sample_3/sample_3.fastq.gz
+sample_4: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/samples/sample_4/sample_4.fastq.gz
 
