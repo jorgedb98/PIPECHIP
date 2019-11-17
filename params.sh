3ac92c4ad635900be5347b063f0fe291718f6e34
saracart <saracart>
2019-11-17 11:32:16 +0100

    cambio WD

--- params.sh
+++ params.sh
@@ -1,9 +1,9 @@
-working_directory: /home/sarajorge/BAG2019/tarea_1/PIPECHIP
+working_directory: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing
 number_of_samples: 4
-genome: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/genome/genome.fa
-annotation: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/annotation/annotation.fa
-sample_1: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/samples/sample_1/sample_1.fastq.gz
-sample_2: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/samples/sample_2/sample_2.fastq.gz
-sample_3: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/samples/sample_3/sample_3.fastq.gz
-sample_4: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/samples/sample_4/sample_4.fastq.gz
+genome: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing/genome/genome.fa
+annotation: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing/annotation/annotation.fa
+sample_1: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing/samples/sample_1/sample_1.fastq.gz
+sample_2: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing/samples/sample_2/sample_2.fastq.gz
+sample_3: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing/samples/sample_3/sample_3.fastq.gz
+sample_4: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing/samples/sample_4/sample_4.fastq.gz
 
