d7f1f8aa6b60bdf0b772572183baae7fa6e4d0c1
saracart <saracart>
2019-11-17 18:27:37 +0100

    procesamiento de las muestras de chip y punto de sincronizacion

--- chip_seq_sample_processing.sh
+++ chip_seq_sample_processing.sh
@@ -1,6 +1,53 @@
-## Author: Sara Cartan Moya and Jorge Dominguez Barragan
-## Date: November 2019
+## Procesamiento de las muestras de Chip
+## Author: Sara Cartan Moya and Jorge Dominguez Barragan 
 
-#! /bin/bash
+## Reading parameters
+
+CHIP_ID=$1
+WD=$2
+NUMCHIP=$3
+SRA=$4
+
+##Acces sample folder 
+
+cd $WD/samples/chip${SAM_ID}
+
+##QC and mapping 
+
+if [ -e {$SRA}_2.fastq ]
+   then
+     fastqc {$SRA}_1.fastq
+     fastqc {$SRA}_2.fastq
+
+     bowtie2 -x ../../genome/index -1 {$SRA}_1.fastq -2 {$SRA}_2.fastq -S chip${$SRA}.sam
+
+   else
+     fastqc {$SRA}_1.fastq
+
+     bowtie2 -x ../../genome/index -U {$SRA}_1.fastq -S chip${SAM_ID}.sam
+fi
+
+## Transcript assembly 
+
+samtools view -S -b chip${SAM_ID}.sam > chip${SAM_ID}.bam
+rm chip${SAM_ID}.sam
+samtools sort chip${SAM_ID}.bam -o chip_sorted_${SAM_ID}.bam
+rm chip${SAM_ID}.bam
+samtools index chip_sorted_${SAM_ID}.bam
+
+
+## Sincronisation poit trough blackboard
+
+echo "chip${SAM_ID} DONE" >> $WD/logs/blackboard
+
+DONE_SAMPLES=$(wc -l $WD/logs/blackboard)
+
+I=0
+if [ $DONE_SAMPLES -eq $NUMCHIP ]
+then 
+   qsub -N input_sample_processing -o $WD/logs/input_sample_processing input_sample_processing.sh PARAMETROS
+   ((I++))
+fi
+
 
 
