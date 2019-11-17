f95bffd3aa01a44baac2260507683f0205583889
saracart <saracart>
2019-11-17 11:57:12 +0100

    modificacion parametros: genome y annotation

--- params.sh
+++ params.sh
@@ -1,8 +1,7 @@
 working_directory: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing
 number_of_samples: 4
-genome_link: ftp://ftp.ensemblgenomes.org/pub/release-45/plants/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz
-genome: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing/genome/genome.fa
-annotation: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing/annotation/annotation.fa
+genome: ftp://ftp.ensemblgenomes.org/pub/release-45/plants/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz
+annotation:ftp://ftp.ensemblgenomes.org/pub/release-45/plants/gtf/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.45.gtf.gz
 sample_1: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing/samples/sample_1/sample_1.fastq.gz
 sample_2: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing/samples/sample_2/sample_2.fastq.gz
 sample_3: /home/sarajorge/BAG2019/tarea_1/PIPECHIP/chip_processing/samples/sample_3/sample_3.fastq.gz
